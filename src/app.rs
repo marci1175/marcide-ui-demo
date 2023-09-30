031500a7afcb41341b116d82d6f7b78e1792b40a
Marcell Varga <v.marcell4000@gmail.com>
2023-09-30 15:25:30 +0200

    yes

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,22 @@
-use egui::{Rect, Color32, Layout, Image, Vec2, vec2};
+use std::{env, path::PathBuf, fs::DirEntry};
+
+use egui::{Rect, Color32, Layout, Image, Vec2, vec2, Frame};
 use image::EncodableLayout;
+use egui_dock::{DockArea, NodeIndex, Style, Tree, DockState};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    app_data : String,
+    #[serde(skip)]
+    files: Vec<DirEntry>,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            app_data: ("C:\\Users\\".to_owned() + &whoami::username() + "\\AppData\\Roaming\\marcimenu"),
-        }
+            files: Vec::new(),
+        } 
     }
 }
 
@@ -28,18 +32,47 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+struct TabViewer<'a> {
+    //tree: &'a mut Tree<usize>,
+    added_nodes: &'a mut Vec<NodeIndex>,
+    ctx: &'a egui::Context,
+    frame: &'a mut eframe::Frame,
+    data: &'a mut TemplateApp,
+}
 
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct AppData {
+    app_data: TemplateApp,
+    #[serde(skip)]
+    tree: Tree<usize>,
+    #[serde(skip)]
+    f11_is_held: bool,
+}
+impl Default for AppData {
+    fn default() -> Self {
+        Self {
+            app_data: TemplateApp::default(),
+            tree: Tree::new(vec![1, 2, 3]),
+            f11_is_held: false,
+        }
     }
+}
+impl AppData {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Load previous app state (if any).
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+impl eframe::App for AppData {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
         egui_extras::install_image_loaders(ctx);
-        
+        let mut added_nodes = Vec::new();
         egui::CentralPanel::default().show(ctx, |ui|{
-
-            
             
             ui.with_layout(Layout::left_to_right(egui::Align::Center), |ui|{
                 ui.allocate_space(vec2(95. , ui.available_height() as f32));
@@ -66,16 +99,68 @@ impl eframe::App for TemplateApp {
 
             ui.with_layout(Layout::top_down(egui::Align::Min), |ui: &mut egui::Ui|{
                 ui.allocate_ui(vec2(80.,80.), |ui|{
-                    ui.menu_image_button(egui::include_image!(), |ui|{
+                    
+                    let file_list = ui.menu_image_button(egui::include_image!("C:\\Users\\marci\\Desktop\\Marcide icons\\files.png"), |ui|{
+                        DockArea::new(&mut self.tree)
+                        .show_close_buttons(true)
+                        //.show_add_buttons(true)
+                        .style({
+                            let mut style = Style::from_egui(ctx.style().as_ref());
+                            style.tab_bar.fill_tab_bar = true;
+                            style
+                        })
+                        .show(
+                            ctx,
+                            &mut TabViewer {
+                                //tree: &mut self.tree,
+                                added_nodes: &mut added_nodes,
+                                ctx,
+                                frame: _frame,
+                                data: &mut self.app_data,
+                            },
+                        );
+                    });
 
-                });
+                    if file_list.response.clicked() {
+
+                        self.app_data.files.clear();
+                        let current_dir = match env::current_dir(){
+                            Ok(ok) => {ok},
+                            Err(_) => {PathBuf::new()},
+                        };
+                        for entry in current_dir.read_dir().expect("read_dir call failed") {
+                            if let Ok(entry) = entry {
+                                self.app_data.files.push(entry);
+                            }
+                        }
+                        for i in &self.app_data.files {
+                            self.tree.push_to_first_leaf(1);
+                        }
+
+                    }
                 
                 });
                 
                 
             });
         });
-            
-        
+    }
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
     }
 }
+
+
+impl egui_dock::TabViewer for TabViewer<'_> {
+    type Tab = usize;
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
+        for i in 0..*tab - 1 {
+            ui.label(self.data.files[i].file_name().to_str().unwrap());
+        }
+    }
+    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
+        format!("file{}",tab).to_string().into()
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,11 @@ pub fn setup() {
         write(icons[i], names[i]);
     }
 }
+
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    setup();
+    
     let native_options = eframe::NativeOptions {
         initial_window_size: Some([400.0, 300.0].into()),
         min_window_size: Some([300.0, 220.0].into()),
@@ -31,7 +32,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "marcimenu",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(eframe_template::AppData::new(cc))),
     )
 }
 
